7b821f55a13011a74ef795b726d7f4744146b841
Taiel Alen Nunes <taielalen56@hotmail.com>
2024-05-05 20:02:41 -0300

    subo ejercicio 14

---
+++ Prácticas/ejercicio_14_AccesoALaBaseDeDatos/src/main/java/ar/edu/unlp/info/oo2/ejercicio_14_AccesoALaBaseDeDatos/DatabaseProxy.java
@@ -0,0 +1,47 @@
+package ar.edu.unlp.info.oo2.ejercicio_14_AccesoALaBaseDeDatos;
+
+import java.util.Collection;
+import java.util.List;
+
+public class DatabaseProxy implements DatabaseAccess{
+	
+	
+	private DatabaseAccess database;
+	private boolean isLog;
+	
+	
+	public DatabaseProxy (DatabaseAccess database) {
+		this.database = database;
+		this.isLog = false;
+	}
+
+	
+	public void logIn () {
+		this.isLog = true;
+	}
+	
+	
+	public void closeSession() {
+		this.isLog = false;
+	}
+	
+	
+	@Override
+	public Collection<String> getSearchResults(String queryString) {
+		if (!this.isLog) {
+			throw new RuntimeException("access denied"); 
+		}
+		return this.database.getSearchResults(queryString);
+	}
+	
+
+	@Override
+	public int insertNewRow(List<String> rowData) {
+		if (!this.isLog) {
+			throw new RuntimeException("access denied"); 
+		}
+		return this.database.insertNewRow(rowData);
+	}
+	
+
+}
