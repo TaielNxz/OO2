4f453b731c0523333a6ebc7724bd5610d5784ba4
Taiel Alen Nunes <taielalen56@hotmail.com>
2024-05-04 21:39:33 -0300

    subo ejercicio 9

---
+++ Prácticas/ejercicio_09_Excursiones/src/main/java/ar/edu/unlp/info/oo2/ejercicio_09_Excursiones/Definitiva.java
@@ -0,0 +1,35 @@
+package ar.edu.unlp.info.oo2.ejercicio_09_Excursiones;
+
+import java.util.List;
+
+public class Definitiva extends Estado {
+
+	public Definitiva(Excursion excursion) {
+		super(excursion);
+	}
+	
+	
+	public void inscribir(Usuario usuario) {
+		
+		Excursion excursion = this.getExcursion();
+		
+		excursion.getInscriptos().add(usuario);
+		
+		if ( excursion.alcanzoCupoMinimo() ) {
+			excursion.setEstado( new Completa(excursion) );
+		}
+		
+	}
+
+
+	public String obtenerInformacion() {
+		
+		Excursion excursion = this.getExcursion();
+		
+		return   "Faltantes para el cupo minimo: " + excursion.faltantesParaCupoMinimo()
+			   + "\nMails de los usuarios inscriptos: " + excursion.mailsDeInscriptos();
+			   
+	}
+
+
+}
