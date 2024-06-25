b54991d97d907236c886eeeec0bcfd12dfe98944
Taiel Alen Nunes <taielalen56@hotmail.com>
2024-06-25 00:44:40 -0300

    subo ejercicio 22 de patrones

---
+++ Practicas/Patrones/ejercicio_22_SubteWay/src/main/java/ar/edu/unlp/info/oo2/ejercicio_22_SubteWay/Sanguche.java
@@ -0,0 +1,28 @@
+package ar.edu.unlp.info.oo2.ejercicio_22_SubteWay;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Sanguche {
+
+	private List<Ingrediente> ingredientes;
+
+	public Sanguche() {
+		this.ingredientes = new ArrayList<Ingrediente>();
+	}
+
+	public List<Ingrediente> getPartes() {
+		return ingredientes;
+	}
+	
+	public double getPrecio() {
+		return this.getPartes().stream()
+				.mapToDouble(parte -> parte.getPrecio())
+				.sum();
+	}
+	
+	public void agregarIngrediente( Ingrediente ingrediente ) {
+		this.ingredientes.add( ingrediente );
+	}
+	
+}
