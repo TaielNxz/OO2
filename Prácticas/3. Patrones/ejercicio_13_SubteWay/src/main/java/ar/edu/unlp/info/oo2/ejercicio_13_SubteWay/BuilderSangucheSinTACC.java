5f930bd984eca0f90eb3e36743eb452fa69cc90f
Taiel Alen Nunes <taielalen56@hotmail.com>
2025-04-28 18:00:21 -0300

    subo ejercico 13 de patrones

---
+++ Prácticas/3. Patrones/ejercicio_13_SubteWay/src/main/java/ar/edu/unlp/info/oo2/ejercicio_13_SubteWay/BuilderSangucheSinTACC.java
@@ -0,0 +1,29 @@
+package ar.edu.unlp.info.oo2.ejercicio_13_SubteWay;
+
+public class BuilderSangucheSinTACC extends BuilderSanguche {
+
+	@Override
+	public void agregarPan() {
+		// TODO Auto-generated method stub
+		this.sanguche.agregarIngrediente(new Ingrediente("Pan de chipá", 150));
+	}
+
+	@Override
+	public void agregarAderezo() {
+		// TODO Auto-generated method stub
+		this.sanguche.agregarIngrediente(new Ingrediente("Salsa tártara", 18));
+	}
+
+	@Override
+	public void agregarPrincipal() {
+		// TODO Auto-generated method stub
+		this.sanguche.agregarIngrediente(new Ingrediente("Carne de pollo", 250));
+	}
+
+	@Override
+	public void agregarAdicional() {
+		// TODO Auto-generated method stub
+		this.sanguche.agregarIngrediente(new Ingrediente("Verduras grilladas", 200));
+	}
+
+}
---
+++ Prácticas/3. Patrones/ejercicio_13_SubteWay/src/main/java/ar/edu/unlp/info/oo2/ejercicio_13_SubteWay/BuilderSangucheVegetariano.java
@@ -0,0 +1,29 @@
+package ar.edu.unlp.info.oo2.ejercicio_13_SubteWay;
+
+public class BuilderSangucheVegetariano extends BuilderSanguche {
+
+	@Override
+	public void agregarPan() {
+		// TODO Auto-generated method stub
+		this.sanguche.agregarIngrediente(new Ingrediente("pan con semillas", 120));
+	}
+
+	@Override
+	public void agregarAderezo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void agregarPrincipal() {
+		// TODO Auto-generated method stub
+		this.sanguche.agregarIngrediente(new Ingrediente("provoleta grillada", 200));
+	}
+
+	@Override
+	public void agregarAdicional() {
+		// TODO Auto-generated method stub
+		this.sanguche.agregarIngrediente(new Ingrediente("berenjenas al escabeche", 100));
+	}
+
+}
