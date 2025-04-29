77cc144231bce1d3b7470eeec9d8ac8898f1f6df
Taiel Alen Nunes <taielalen56@hotmail.com>
2025-04-29 20:47:47 -0300

    subo ejercico 16 de patrones

---
+++ Prácticas/3. Patrones/ejercicio_16_FiltrosDeImagenes/src/main/java/ar/edu/unlp/info/oo2/ejercicio_16_FiltrosDeImagenes/imageFilters/Dull.java
@@ -0,0 +1,11 @@
+package ar.edu.unlp.info.oo2.ejercicio_16_FiltrosDeImagenes.imageFilters;
+
+import java.awt.image.BufferedImage;
+
+public class Dull extends Filter {
+    
+  public BufferedImage filter(BufferedImage image) {
+    return image;
+  }
+  
+}
---
+++ Prácticas/3. Patrones/ejercicio_16_FiltrosDeImagenes/src/main/java/ar/edu/unlp/info/oo2/ejercicio_16_FiltrosDeImagenes/imageFilters/Filter.java
@@ -0,0 +1,9 @@
+package ar.edu.unlp.info.oo2.ejercicio_16_FiltrosDeImagenes.imageFilters;
+
+import java.awt.image.BufferedImage;
+
+public abstract class Filter {
+	
+  abstract public BufferedImage filter(BufferedImage image);
+  
+}
